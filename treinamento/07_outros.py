32b8f29c17352c349a408f1f146fcefeea66183f
Antonio Felipe Cechi Neto <pronetao@gmail.com>
2018-02-22 07:07:39 -0300

    Novos

---
+++ treinamento/07_outros.py
@@ -0,0 +1,9 @@
+# Existem muitas bibliotecas prontas em python
+# para instalar, basta usar o comando pip install <biblitoteca>
+# para importar, basta usar o import
+
+import io, sys
+# É possivel importar classes expecíficas
+from datetime import datetime
+# ou importar tudo
+from io import *
