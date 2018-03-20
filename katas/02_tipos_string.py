7c592546a6e64452a750a7fb9fba9df55751d96d
Smart Presence CPS <sa_smart_cps@cit>
2018-03-20 08:18:14 -0300

    Exercicios

---
+++ katas/02_tipos_string.py
@@ -0,0 +1,27 @@
+# Escreva um método que retorne o tamanho de uma string
+
+def tamanho_string(str):
+    return None
+
+assert tamanho_string('neto') == 4
+
+# Escreva um método que retorne uma string formada pelos dois primeiros caractes e os dois ultimos
+
+def nova_string(str):
+    return None
+
+assert nova_string('Mirandamente') == 'Mite'
+
+# Escreva um método que troque a primeira letra de duas palavras, separadas por espaço
+def troca_letra(str):
+    return None
+
+assert troca_letra('Primeira Frase') == 'Frimeira Prase'
+
+# Escreva um método que remova todos os caracteres em indice impar
+
+def remove_impar(str):
+    return None
+
+assert remove_impar('UmaString') == 'Uatig'
+
