1757c9f066af77bb5c34b62e2bab28f6a3bad5df
Smart Presence CPS <sa_smart_cps@cit>
2018-03-20 08:17:19 -0300

    Exercicios

---
+++ katas/01_tipos_numericos.py
@@ -0,0 +1,26 @@
+# Escreva um método que converte graus para radianos
+def graus_radianos(num):
+    ''' Um radiano tem 57.3 graus '''
+    return None
+
+assert graus_radianos(15) == 0.2619047619047619
+
+#Escreva um método que calcule a área de um trapézio
+def area_trapesio(altura, base_maior, base_menor):
+    ''' google a formula '''
+    return None 
+
+assert area_trapesio(5, 6, 5) == 27.5
+
+# Escreva um método que encontre todos os divisores comuns de um número
+def divisor_comum(num):
+    return []
+
+assert divisor_comum(8) == [1, 2, 4]
+
+# Escreva um método que converte um número para binário
+
+def dec_binario(num):
+    return None 
+
+assert dec_binario(43) == 0b101011
---
+++ katas/03_tipos_sequenciais.py
@@ -0,0 +1,29 @@
+# Escreva um método que some todos os itens de uma lista
+def soma_lista(lst):
+    return None
+
+assert soma_lista([1,2,3]) == 6
+
+# Escreva um método que retorne o maior numero de uma lista
+
+def maior_numero(lst):
+    return None 
+
+assert maior_numero([1,5,8,10,2,3]) == 10
+
+# Escreva um método que remove itens duplicados de uma lista
+def remove_duplicados(lst):
+    return None
+
+assert remove_duplicados([1,1,2,2,2,3,3,3,3,4,4,5,6]) == [1,2,3,4,5,6]
+
+# Escreva um método que retorne um item aleatório de uma lista
+def item_aleatorio(lst):
+    return None
+
+
+# Escreva um método que converte uma lista de inteiros em um unico inteiro
+def unico_inteiro(lst):
+    return None
+
+assert unico_inteiro([11,33,55]) == 113355
